55d623b68970bc5ef74069ba023ca1e983f1e3fd
fuchsberger <admin@fuchsberger.us>
2019-09-20 20:07:50 -0400

    added heatmap test

--- assets/js/views/page/test.js
+++ assets/js/views/page/test.js
@@ -1,4 +1,5 @@
 import $ from 'jquery'
+import h337 from 'heatmap.js'
 import MainView from '../main'
 
 // draws the canvas content
@@ -15,17 +16,43 @@ const resize = () => {
   let canvas = $('#heatmap')[0]
   canvas.width = window.innerWidth
   canvas.height = window.innerHeight
-  drawHeatmap(canvas)
+  // drawHeatmap(canvas)
 }
 
 export default class View extends MainView {
   mount() {
     super.mount()
 
-    // resize canvas and draw it for the first time
-    resize()
+    // configure heatmap
+    var config = {
+      container: $('body')[0],
+      radius: 45,
+      maxOpacity: .8,
+      minOpacity: 0,
+      blur: .75
+    }
 
-    // listen vor page resize event
+    // create heatmap with configuration
+    var heatmapInstance = h337.create(config)
+
+    // create data points
+    let datapoints = []
+    for(let i=0; i<500; i++){
+      datapoints.push({
+        x: Math.floor(Math.random() * window.innerWidth),
+        y: Math.floor(Math.random() * window.innerHeight),
+        value: Math.floor(Math.random() * 101)
+      })
+    }
+
+    var data = { max: 100, min: 0, data: datapoints }
+
+    heatmapInstance.setData(data)
+
+    // resize canvas and draw it for the first time
+    // resize()
+
+    // listen for page resize event
     window.addEventListener("resize", () => {
       if(this.resizeTimeout) clearTimeout(this.resizeTimeout);
       this.resizeTimeout = setTimeout(resize, 200);
